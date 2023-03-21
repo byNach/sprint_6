88cf6b92a4398f0ba9e11202a468317544a5cd1a
nach <nachnitror@gmail.com>
2023-03-21 10:18:07 +0100

    ADD contador 0-3

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,18 +1,40 @@
 import './App.css';
 import Scene from './components/Scene';
 import Strings from './components/Text';
-import Pstyle from './styled-p';
 import Divstyle from './styled-div';
+import Buttonstyle from './styled-button';
+import { useState } from 'react';
 
 function App() {
+
+  const [num, setNum] = useState(0);
+  const anterior = () => {
+    if (num === 0) {
+      setNum(3)
+    }
+    else {
+      setNum(num - 1);
+    }
+  };
+  const posterior = () => {
+    if (num === 3) {
+      setNum(0)
+    }
+    else {
+      setNum(num + 1);
+    }
+  };
+
   return (
     <Divstyle>
-      <Pstyle><Scene phrase={Strings[0]} /></Pstyle>
-      <Pstyle><Scene phrase={Strings[1]} /></Pstyle>
-      <Pstyle><Scene phrase={Strings[2]} /></Pstyle>
-      <Pstyle><Scene phrase={Strings[3]} /></Pstyle>
+      <h1> {num} </h1>
+      <Buttonstyle onClick={anterior}>Anterior</Buttonstyle>
+      <Buttonstyle onClick={posterior}>Següent</Buttonstyle>
+      <Scene phrase={Strings[0]} />
+      <Scene phrase={Strings[1]} />
+      <Scene phrase={Strings[2]} />
+      <Scene phrase={Strings[3]} />
     </Divstyle>
-    
   );
 }
 
