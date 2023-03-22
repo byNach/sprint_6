495041100f7a1c3a3b2fadbf143616acc2fda702
nach <nachnitror@gmail.com>
2023-03-22 10:34:27 +0100

    ADD PstyleBg

---
+++ intro/src/styles/styled-p.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Pstyle = styled.p`
+    font-size: 26px;
+    border: 2px solid black;
+    border-radius: 90px;
+    display: flex;
+    justify-content: center;
+    max-width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 30px;
+`
+export default Pstyle;
