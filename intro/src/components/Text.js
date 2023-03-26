059f3f16842a69a46de4fb78637451c3a4f77d8b
nach <nachnitror@gmail.com>
2023-03-26 14:29:08 +0200

    C directory images

--- intro/src/components/Text.js
+++ intro/src/components/Text.js
@@ -1,7 +1,7 @@
 const Strings = [
   {
     text: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial", 
-    img: "../images/1.jpg"
+    img: "public/images/1.jpg"
   },
   {
     text: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
