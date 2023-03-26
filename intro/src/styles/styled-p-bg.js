a2f2862899c7f02ccfe9dd9c6e5dc491d5293fa8
nach <nachnitror@gmail.com>
2023-03-26 10:44:35 +0200

    RF identation

--- intro/src/styles/styled-p-bg.js
+++ intro/src/styles/styled-p-bg.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
 const PstyleBg = styled.p`
-    font-size: 26px;
-    border: 2px solid black;
-    border-radius: 90px;
-    display: flex;
-    justify-content: center;
-    max-width: 80%;
-    margin-left: auto;
-    margin-right: auto;
-    padding: 30px;
-    background-color: red;
-    color: white;
+  font-size: 26px;
+  border: 2px solid black;
+  border-radius: 90px;
+  display: flex;
+  justify-content: center;
+  max-width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 30px;
+  background-color: red;
+  color: white;
 `
 export default PstyleBg;
